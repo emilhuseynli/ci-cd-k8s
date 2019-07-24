a9a91ec0da2fd251e8ad5421ead53eb91bee1208
gshulman <gshulman@urbanscience.com>
2019-07-24 23:58:56 +0200

    Added URI /sysinfo

--- app/server.js
+++ app/server.js
@@ -2,8 +2,10 @@ var restify = require("restify");
 var server = restify.createServer();
 var pkg = require("../package.json");
 var rootResponder = require("./routes/root");
+var sysinfoResponder = require("./routes/sysinfo");
 
 server.get("/", rootResponder);
+server.get("/sysinfo", sysinfoResponder);
 
 server.listen(3000, function() {
   console.log("restifyjs version %s running on port 3000", pkg.version);
